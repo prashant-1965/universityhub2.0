099ee2a7179a7e8908c75e6199703734b1528aaf
Prashant Bhardwaj <122390182+prashant-1965@users.noreply.github.com>
2025-07-31 16:18:25 +0530

    Delete universitySoftware directory

--- universitySoftware/src/main/java/com/university/security/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.university.security.service;
-
-import com.university.security.entity.Credentials;
-
-public interface UserService {
-    boolean addCredentials(Credentials credentials);
-    boolean existByUsername(String username);
-}
